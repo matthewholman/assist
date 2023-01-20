3645d01c20a1ad4626ef3ed09e09e667e53cdb58
Hanno Rein <hanno@hanno-rein.de>
2023-01-20 16:41:30 -0500

    added more complicated example

--- examples/plain_interface_substeps/problem.c
+++ examples/plain_interface_substeps/problem.c
@@ -32,6 +32,7 @@ int main(int argc, char* argv[]){
     // Final integration time (relative to ax->jd_ref)
     double tend = 7404.5;  
 
+    ///////////////////////////////////////////
     // Generate outputs with a fixed interval
     double next_output = r->t; 
     double output_interval = 8; 
@@ -39,7 +40,7 @@ int main(int argc, char* argv[]){
 
     // Integrate until we reach tend or an error occurs
     while (r->t < tend && r->status <= 0){
-        reb_integrate(r, next_output); // This might overshoot.
+        reb_integrate(r, next_output); // Do the actual integration. This might overshoot.
 
         while (r->t >= next_output) { // do interpolation to get outputs
             printf("t = %.3f     steps = %d\n", next_output,r->steps_done);
@@ -51,6 +52,35 @@ int main(int argc, char* argv[]){
             next_output += output_interval;
         }
     }
+
+    /////////////////////////////////////////////////////////////
+    // Generate outputs at nodes for Chebyshev interpolation 
+    double interval = 30;
+    tend += 200;
+    const int N = 10;
+    double nodes[N];
+    for (int k=0; k<N; k++){
+        nodes[k] = cos((2.0*k+1.0)*M_PI/(2.0*N))/2.0 + 0.5;
+    } 
+    int counter = 0; // steps through 0 to N-1
+    
+
+    // Integrate until we reach tend or an error occurs
+    while (r->t < tend && r->status <= 0){
+        reb_integrate(r, next_output); // Do the actual integration. This might overshoot.
+
+        while (r->t >= next_output) { // do interpolation to get outputs
+            printf("t = %.3f     steps = %d       counter = %d\n", next_output, r->steps_done, counter);
+            double h = (next_output - (r->t - r->dt_last_done))/ r->dt_last_done;
+            assist_interpolate(r, h, output_state);
+            for(int i=0; i<r->N; i++){
+                printf("\tx = %.12f \ty = %.12f \tz = %.12f\n", output_state[i*6+0], output_state[i*6+1], output_state[i*6+2]);
+            }
+            next_output += interval*nodes[counter];
+            counter = (counter+1)%N;
+        }
+    }
+
 
     // Clean up memory
     assist_free(ax);
