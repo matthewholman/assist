0bc687a574f0760a0c3f0871d596ccc3cef4a8a9
Hanno Rein <hanno@hanno-rein.de>
2023-02-04 11:12:51 -0500

    force selection in python works

--- assist/extras.py
+++ assist/extras.py
@@ -4,6 +4,18 @@ import rebound
 import warnings
 from .ephem import Ephem
 
+ASSIST_FORCES = {
+    "SUN"                : 0x01,
+    "PLANETS"            : 0x02,
+    "ASTEROIDS"          : 0x04,
+    "NON_GRAVITATIONAL"  : 0x08,
+    "EARTH_HARMONICS"    : 0x10,
+    "SUN_HARMONICS"      : 0x20,
+    "GR_EIH"             : 0x40,
+    "GR_SIMPLE"          : 0x80,
+    "GR_POTENTIAL"       : 0x100,
+} 
+
 class Extras(Structure):
     """
     Main object used for all ASSIST operations, tied to a particular REBOUND simulation.
@@ -24,6 +36,28 @@ class Extras(Structure):
 
     def integrate_or_interpolate(self, t):
         clibassist.assist_integrate_or_interpolate(byref(self), c_double(t))
+    
+    @property
+    def forces(self):
+        l = []
+        for k in ASSIST_FORCES:
+            if self._forces & ASSIST_FORCES[k]:
+                l.append(k)
+        return l
+    @forces.setter
+    def forces(self, value):
+        if not isinstance(value, list):
+            raise AttributeError("Forces need to be a list.")
+        for v in value:
+            if not isinstance(v, str):
+                raise AttributeError("Each force needs to be a string.")
+            if v.upper() not in ASSIST_FORCES:
+                raise AttributeError("Force '"+v+"' not recognized. Needs to be one of the following: "+", ".join(ASSIST_FORCES))
+        v = 0
+        for k in ASSIST_FORCES:
+            if k in value:
+                v = v | ASSIST_FORCES[k]
+        self._forces = c_int(v)
 
     _fields_ =  [("_sim", POINTER(rebound.Simulation)),
                  ("ephem", POINTER(Ephem)),
@@ -34,7 +68,7 @@ class Extras(Structure):
                  ("current_state", POINTER(rebound.Particle)),
                  ("particle_params", c_void_p),
                  ("steps_done", c_int),
-                 ("forces", c_int),
+                 ("_forces", c_int),
         ]
 
 # avoid circular imports
