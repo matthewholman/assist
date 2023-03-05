4c5cbdb47605cbd1b270d8b1abb3e4f871d3ccf3
Hanno Rein <hanno@hanno-rein.de>
2023-03-05 16:51:57 -0500

    better

--- src/spk.c
+++ src/spk.c
@@ -65,92 +65,84 @@ int assist_spk_free(struct spk_s *pl)
 static double inline _jul(double eph)
 	{ return 2451545.0 + eph / 86400.0; }
 
+
+#define record_length 1024
 // check for any non-7bit ascii characters
-static int _com(const char *buf)
-{
-	for (int n = 0; n < 1024; n++)
-		{ if (buf[n] < 0) return 0; }
+static int _com(const char *record) {
+	for (int n = 0; n < record_length; n++)
+		{ if (record[n] < 0) return 0; }
 
 	return 1;
 }
 
-// display output strings to console
-//static void _sho(const char *buf)
-//{
-//	int n;
-//
-//	// this doesn't always handle the newlines correctly
-//	for (n = 0; buf[n+1] != '\0';)
-//		n += fprintf(stdout, "%s\n", &buf[n]) - 1;
-//}
+struct spk_s * assist_spk_init(const char *path) {
+    // For file format, see https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/FORTRAN/req/daf.html
 
-struct spk_s * assist_spk_init(const char *path)
-{
 	struct stat sb;
-	char buf[1024];
-	int fd, nd, ni, nc;
-	int m, n, c, b, B;
+
+    // We will read one record at a time.
+    union {
+	    char buf[record_length];
+        struct {
+            double next;
+            double prev;
+            double nsum;
+        } summary;          // Summary record
+        struct {
+            char locidw[8]; // An identification word
+            int nd;         // The number of double precision components in each array summary.
+            int ni;         // The number of integer components in each array summary.
+        } file;             // File record
+    } record;
+
+	int fd;
 	off_t off;
 
-	if ((fd = open(path, O_RDONLY)) < 0)
+	if ((fd = open(path, O_RDONLY)) < 0){
 		return NULL;
+    }
 
-	struct spk_s* pl = calloc(1, sizeof(struct spk_s));
-	double* val = (double *)buf;
-	struct sum_s* sum = (struct sum_s *)buf;
+	double* val = (double *)record.buf;
+	struct sum_s* sum = (struct sum_s *)record.buf;
 
 	if (fstat(fd, &sb) < 0)
 		goto err;
 
-	// LOCIDW
-	if (read(fd, buf, 8) != 8)
-		goto err;
-
-	if (strncmp(buf, "DAF/SPK", 7) != 0) {
-		errno = EILSEQ;
-		goto err;
+	// Read the file record. 
+    read(fd, &record, 1024);
+    // Check if the file is a valid Double Precision Array File
+	if (strncmp(record.file.locidw, "DAF/SPK", 7) != 0) {
+        fprintf(stderr,"Error parsing DAF/SPK file. Incorrect header.\n");
+		close(fd);
+		return NULL;
 	}
 
-	// ND, NI
-	read(fd, &nd, sizeof(int));
-	read(fd, &ni, sizeof(int));
-
-	// length of each segment, must match our sum_s struct
-	nc = 8 * ( nd + (ni + 1) / 2 );
-
+    // Check that the size of a summary record is equal to the size of our struct.
+	int nc = 8 * ( record.file.nd + (record.file.ni + 1) / 2 );
 	if (nc != sizeof(struct sum_s)) {
-		errno = EILSEQ;
-		goto err;
+        fprintf(stderr,"Error parsing DAF/SPK file. Wrong size of summary record.\n");
+		close(fd);
+		return NULL;
 	}
+    
+    // Continue reading file until we find a non-ascii record.
+    do {
+		read(fd, record.buf, 1024);
+    } while (_com(record.buf) > 0);
 
-	// could check the other headers, but we really don't care
-	// so find the first summary record (after potential comments)
-	off = lseek(fd, 1024, SEEK_SET);
-	read(fd, buf, 1024);
-
-	while (_com(buf) > 0) {
-	//	_sho(buf);
-		off = lseek(fd, 0, SEEK_CUR);
-		read(fd, buf, 1024);
-	}
-
-	// we are at the first summary block, validate
-	if (val[1] != 0.0) {
+	// We are at the first summary block, validate
+	if ((int64_t)record.buf[8] != 0) {
 		errno = EILSEQ;
 		goto err;
 	}
 
 	// okay, let's go
-	m = 0;
-next:	n = (int)val[0] - 1;
-	B = (int)val[2];
-
-	for (b = 0; b < B; b++) {
-		sum = (struct sum_s *)&buf[24 + b * sizeof(struct sum_s)];
-
-//		fprintf(stdout, "beg %.1f end %.1f tar %d cen %d ref %d ver %d one %d two %d\n",
-//				_jul(sum->beg), _jul(sum->end), sum->tar, sum->cen,
-//				sum->ref, sum->ver, sum->one, sum->two);
+	struct spk_s* pl = calloc(1, sizeof(struct spk_s));
+	int c ;
+	int m = 0;
+next:
+	for (int b = 0; b < (int)record.summary.nsum; b++) {
+		sum = (struct sum_s *)&record.buf[24 + b * sizeof(struct sum_s)];
 
 		// pick out new target!
 		if (sum->tar != pl->tar[m]) {
@@ -170,10 +162,12 @@ next:	n = (int)val[0] - 1;
 		pl->end[m] = _jul(sum->end);
 	}
 
+    // Location of next record
+    int n = (int)record.summary.next - 1;
 	if (n >= 0) {
 		// this could probably be more elegant if the mmap happened sooner
 		off = lseek(fd, n * 1024, SEEK_SET);
-		read(fd, buf, 1024);
+		read(fd, record.buf, 1024);
 		goto next;
 	}
 
